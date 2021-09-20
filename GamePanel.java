d8ca8b6796f94c1e734138ef9a49cf7552307c4e
janmmeier <Jan-Markus.Meier@tptlive.ee>
2021-09-20 13:23:07 +0300

    Apple randomizer

--- GamePanel.java
+++ GamePanel.java
@@ -8,9 +8,9 @@ import java.util.Random;
 public class GamePanel extends JPanel implements ActionListener{
 
     static final int SCREEN_WIDTH = 600;
-    static final int SCREE_HEIGHT = 600;
+    static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
-    static final int GAME_UNITS = (SCREEN_WIDTH * SCREE_HEIGHT) / UNIT_SIZE;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
     static final int DELAY = 75;
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
@@ -25,7 +25,7 @@ public class GamePanel extends JPanel implements ActionListener{
 
     GamePanel() {
         random = new Random();
-        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREE_HEIGHT));
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.black);
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
@@ -44,10 +44,18 @@ public class GamePanel extends JPanel implements ActionListener{
 
     }
     public void draw(Graphics g) {
+        //screening for Apple placement
+        for(int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE;i++) {
+            g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+        }
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
     }
     public void newApple() {
-
+        appleX = random.nextInt((int)(SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
     }
     public void move() {
 
